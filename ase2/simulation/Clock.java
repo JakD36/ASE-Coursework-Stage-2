87ed0930bfbbec6348f402d8f4a0d77a9526c9a7
Jack Davidson <jakd36@gmail.com>
2018-03-14 14:57:19 +0000

    Added simulation clock
    incorporated log singleton into the rest of the application

---
+++ ase2/simulation/Clock.java
@@ -0,0 +1,58 @@
+package ase2.simulation;
+
+public class Clock 
+{
+    long startTime;
+    long lastTime; // Time at last call
+    long lastSimTime;
+    long startSimTime = 6*3600*1000; // Start the simulation at 6 am
+    long speed = 10000;
+
+
+    //Static variable keeping the singleton instance of the class
+	static private Clock instance;
+	/*
+	 * Creates a new instance of the class. It's private to allow the Singleton D.P.
+	 */
+	private Clock() {
+        lastTime = System.currentTimeMillis();
+        lastSimTime = startSimTime;
+	}
+    
+    public synchronized long getCurrentTime(){
+        long currentRealTime = System.currentTimeMillis();
+        long elapsedTime = (currentRealTime - lastTime); // elapsed time from last call
+        long currentSimTime = lastSimTime + speed*elapsedTime;
+        lastSimTime = currentSimTime;
+        lastTime = currentRealTime;
+        return currentSimTime;
+    }
+    public synchronized long getSpeed(){
+        return speed;
+    }
+
+	public synchronized String getTimeString(){
+        long simTime = getCurrentTime();
+		long ms = simTime%1000;
+        long s = (simTime / 1000) % 60 ;
+        long min = (simTime / (1000*60)) % 60;
+		long hour = (simTime / (1000*60*60)) % 24;
+		return hour+":"+min+":"+s+":"+ms;
+	}
+    
+    public synchronized void setSpeed(long speed){
+        getCurrentTime(); // updates the lastSimTime, so we are change the speed for only the time moving forward
+        this.speed = speed;
+    }
+	/*
+	 * 
+	 */
+	synchronized static public Clock getInstance(){ // use double locking
+		if (Clock.instance == null){
+			synchronized(Clock.class) { // lock block if (instance == null) // and re-check
+                instance = new Clock(); 
+            }
+		}
+		return Clock.instance;
+	}
+}
---
+++ ase2/simulation/Logging.java
@@ -0,0 +1,90 @@
+package ase2.simulation;
+
+import java.io.BufferedWriter;
+import java.io.File;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.util.ArrayList;
+
+public class Logging 
+{
+	//List of recorded events
+	private ArrayList<String> events;
+	
+	//Events counter
+	private int counter = 1;
+	
+	//Constant keeping the filename for the log file
+	static private final String FILENAME = "logging.txt";
+	
+	//Static variable keeping the singleton instance of the class
+	static private Logging instance;
+	
+	/*
+	 * Creates a new instance of the class. It's private to allow the Singleton D.P.
+	 */
+	private Logging() {
+		this.events = new ArrayList<String>();
+	}
+	
+	/*
+	 * Adds a new event in the list
+	 * 
+	 * event: Event to append
+	 */
+	public void writeEvent(String event)
+	{
+		synchronized(this.events) {
+			events.add(String.format("Event #%d: %s"+ System.lineSeparator() , this.counter,event));
+			this.counter++;
+		}
+	}
+	
+	/*
+	 * Returns the current number of recorded events
+	 * 
+	 * Return: number of the current events
+	 */
+	public int getNumberOfEvents()
+	{
+		return events.size();
+	}
+	
+	/*
+	 * Writes all the events in the logging file
+	 */
+	public void flush() throws IOException 
+	{
+		StringBuilder event_str = new StringBuilder();
+		
+		synchronized(this.events) 
+		{
+			for (String e : this.events) {
+				event_str.append(e);
+			}
+			
+			File logFile = new File(Logging.FILENAME);
+	
+			BufferedWriter writer = new BufferedWriter(new FileWriter(logFile,true));
+		    writer.write(event_str.toString());
+		    writer.close();
+		    
+		    this.events.clear();
+		}
+	
+	}
+	
+	
+	/*
+	 * Returns the Singleton instance of the Logging class
+	 */
+	synchronized static public Logging getInstance()
+	{
+		if (Logging.instance == null)
+		{
+			Logging.instance = new Logging();
+		}
+		
+		return Logging.instance;
+	}
+}
