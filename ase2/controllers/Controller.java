a5131c881a43f4235e8bb2c82e982ba4b250ebc9
Daniel <Danielmclaren06@gmail.com>
2018-03-18 19:12:02 +0000

    Implemented flight time for Sprint 3

--- ase2/controllers/Controller.java
+++ ase2/controllers/Controller.java
@@ -3,11 +3,17 @@ package ase2.controllers;
 import java.util.ArrayList;
 import java.util.LinkedList;
 import java.util.NoSuchElementException;
+
+import javax.swing.JFrame;
+import javax.swing.JOptionPane;
+
 import java.awt.event.ActionListener;
+import java.io.IOException;
 import java.awt.event.ActionEvent;
 
 import ase2.interfaces.Observer;
 import ase2.interfaces.Subject;
+import ase2.simulation.Logging;
 import ase2.simulation.Simulation;
 import ase2.views.GUI;
 import ase2.model.Passenger;
@@ -23,6 +29,27 @@ public class Controller{
         // Specify the listener for the view
         view.addStartListener( new StartListener() );
         // TODO add update Listener in sprint 3
+        
+        
+        //adding an event on closing the view to ensure the log flush
+        view.setDefaultCloseOperation(JFrame.DO_NOTHING_ON_CLOSE);
+        view.addWindowListener(new java.awt.event.WindowAdapter() {
+		    @Override
+		    public void windowClosing(java.awt.event.WindowEvent windowEvent) {
+		        if (JOptionPane.showConfirmDialog(view, 
+		            "Are you sure to quit the simulation?", "Really Closing?", 
+		            JOptionPane.YES_NO_OPTION,
+		            JOptionPane.QUESTION_MESSAGE) == JOptionPane.YES_OPTION){
+		        		try {
+							Logging.getInstance().flush();
+						} catch (IOException e) {
+							// TODO Auto-generated catch block
+							e.printStackTrace();
+						}
+		            System.exit(0);
+		        }
+		    }
+		});
     }
 
     // inner class SetListener responds when user sets time
